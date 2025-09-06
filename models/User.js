4f68e9ecf11d332f87e020a726213466ef9de889
098ff <chanatdakc@gmail.com>
2025-09-06 15:45:59 +0700

    JWT

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -40,5 +41,15 @@ UserSchema.pre('save', async function(next) {
     // Hashing salt within our password
     this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken=function() {
+    // Bring our data to encrypt with JWT
+    return jwt.sign(
+        {id: this._id},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRE}
+    );
+}
 
 module.exports = mongoose.model('User', UserSchema);
